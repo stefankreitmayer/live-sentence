b329f9a491dfb7b5e1df99f06629f1c4791e65bf
stefankreitmayer <stefankreitmayer@gmail.com>
2016-08-03 09:59:50 +0200

    add dummy parts

--- web/elm/src/Model.elm
+++ web/elm/src/Model.elm
@@ -1,11 +1,18 @@
 module Model exposing (..)
 
+import String
+
 import Model.Ui exposing (..)
 
 
 type alias Model =
   { ui : Ui
-  , sentence : List Atom }
+  , sentence : List Atom
+  , parts : List Part }
+
+type alias Part =
+  { name : String
+  , atoms : List Atom }
 
 type alias Atom = String
 
@@ -13,4 +20,19 @@ type alias Atom = String
 initialModel : Model
 initialModel =
   { ui = initialUi
-  , sentence = [ "The brown fox", "jumps", "over an incredible number of fences", "like there's no tomorrow" ] }
+  , sentence = initialSentence dummyParts
+  , parts = dummyParts }
+
+
+dummyParts : List Part
+dummyParts =
+  [ Part "subject" (String.split "," "the dog,the goose,nobody")
+  , Part "verb" (String.split "," "chases,ignores")
+  , Part "object" (String.split "," "butterflies,dreams,you")
+  , Part "rest" (String.split "," "around the house,tonight,forever") ]
+
+
+initialSentence : List Part -> List Atom
+initialSentence parts =
+  parts
+  |> List.map (\part -> List.head part.atoms |> Maybe.withDefault "-")
