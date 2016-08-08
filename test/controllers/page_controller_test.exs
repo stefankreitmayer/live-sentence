996d1902e911e4ed27c534b69497afaddebfd51a
stefankreitmayer <stefankreitmayer@gmail.com>
2016-08-08 23:12:37 +0200

    remove unneeded tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LiveSentence.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Elm.Main.fullscreen()"
   end
 end
